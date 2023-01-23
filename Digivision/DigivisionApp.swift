0a6e0fefdc8e57ce48f7e0c2342462170abfa1ce
ahmadsalimi <ahsa9978@gmail.com>
2023-01-23 20:41:57 +0330

    Initial Commit

---
+++ Digivision/DigivisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigivisionApp.swift
+//  Digivision
+//
+//  Created by Ahmad on 11/3/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct DigivisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
